fb76957585f77a3c6cd022a3c26d4d41863578a9
Ahmad Jamal Khan <ahmadjamalit@gmail.com>
2024-09-28 02:48:24 +0500

    Todo App

---
+++ todoApp/src/context/ToDoContext.js
@@ -0,0 +1,20 @@
+import { useContext, createContext } from "react";
+
+export const TodoContext = createContext({
+  todos: [
+    {
+      id: 1,
+      todo: "todo msg",
+      completed: true,
+    },
+  ],
+  addTodo:(todo)=>{},
+  updateTodo:(id,todo)=>{},
+  deleteTodo:(id)=>{},
+  toggleTodo:(id)=>{}
+});
+
+export const useTodo = () => {
+  return useContext(TodoContext);
+};
+export const TodoProvider = TodoContext.Provider;
